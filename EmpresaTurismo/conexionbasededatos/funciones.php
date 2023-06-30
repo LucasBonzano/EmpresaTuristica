fb219dd8a3c87ffea81e9541c73200ffe3fbc389
Musso21 <107602755+Musso21@users.noreply.github.com>
2023-06-30 10:22:41 -0300

    Add files via upload

---
+++ EmpresaTurismo/conexionbasededatos/funciones.php
@@ -0,0 +1,43 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "formulario";
+
+// Establecer conexión a la base de datos
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verificar errores en la conexión a la base de datos
+if ($connection->connect_error) {
+    die("Error de conexión: " . $connection->connect_error);
+}
+// Verificar si hay errores en la conexión
+if (!$connection) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+// Recibir datos enviados desde la solicitud POST en formato JSON
+$data = json_decode(file_get_contents('php://input'), true);
+$nombre = $data['nombre'];
+$apellido = $data['apellido'];
+$mail = $data['mail'];
+$dni = $data['dni'];
+$consulta = $data['consulta'];
+
+// Realizar la inserción de datos en la base de datos
+$insertQuery = "INSERT INTO contacto(nombre, apellido, mail, dni, consulta, id) VALUES ('$nombre', '$apellido', '$mail', '$dni', '$consulta', '')";
+mysqli_query($connection, $insertQuery);
+
+// Cerrar la conexión a la base de datos
+mysqli_close($connection);
+
+// Respuesta al cliente (puedes personalizarla según tus necesidades)
+$response = array('status' => 'success', 'message' => 'Datos insertados correctamente');
+echo json_encode($response);
+?>
--- EmpresaTurismo/js/script.js
+++ EmpresaTurismo/js/script.js
@@ -8,3 +8,30 @@ submenus.forEach((submenu) => {
     submenu.classList.toggle("active");
   });
 });
+
+// Datos que deseas enviar al archivo PHP
+const datosEnviar = {
+  nombre: 'John',
+  apellido: 'Doe',
+  mail: 'johndoe@example.com',
+  dni: '123456789',
+  consulta: 'Consulta de ejemplo'
+};
+
+fetch('../../conexionbasededatos/funciones.php', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*' // Configuración de la cabecera CORS
+  },
+  body: JSON.stringify(datosEnviar)
+})
+.then(response => response.json())
+.then(data => {
+  // Manejar la respuesta del servidor
+  console.log(data);
+})
+.catch(error => {
+  // Manejar errores de la solicitud
+  console.error(error);
+});
