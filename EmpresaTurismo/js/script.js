a3093713964d3f94e3f7c1024035bea5e2805050
Lucas Bonzano <86752246+LucasBonzano@users.noreply.github.com>
2023-09-14 11:12:23 -0300

    solución

--- EmpresaTurismo/js/script.js
+++ EmpresaTurismo/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:js/script.js
 document.addEventListener("DOMContentLoaded", function() {
   const submenus = document.querySelectorAll(".submenu");
 
@@ -11,18 +10,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-  const formulario = document.getElementById("formulario");
+  var formulario = document.getElementById('formulario');
 
   formulario.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const mail = document.getElementById("mail").value;
-    const dni = document.getElementById("dni").value;
-    const consulta = document.getElementById("consulta").value;
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var mail = document.getElementById("mail").value;
+    var dni = document.getElementById("dni").value;
+    var consulta = document.getElementById("consulta").value;
 
-    const datosEnviar = {
+    var datosEnviar = {
       nombre: nombre,
       apellido: apellido,
       mail: mail,
@@ -148,188 +147,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
-=======
-document.addEventListener("DOMContentLoaded", function() {
-  const submenus = document.querySelectorAll(".submenu");
-
-  submenus.forEach((submenu) => {
-    const parentLink = submenu.previousElementSibling;
-
-    parentLink.addEventListener("click", (event) => {
-      event.preventDefault();
-      submenu.classList.toggle("active");
-    });
-  });
-
-  const formulario = document.getElementById("formulario");
-
-  formulario.getElementById('subir').addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const mail = document.getElementById("mail").value;
-    const dni = document.getElementById("dni").value;
-    const consulta = document.getElementById("consulta").value;
-
-    const datosEnviar = {
-      nombre: nombre,
-      apellido: apellido,
-      mail: mail,
-      dni: dni,
-      consulta: consulta
-    };
-
-    fetch('../../conexionbasededatos/insert.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(datosEnviar)
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      formulario.reset(); // Restablecer el formulario después de enviar los datos
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  });
-
-  formulario.addEventListener("update", (event) => {
-    event.preventDefault();
-
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const mail = document.getElementById("mail").value;
-    const dni = document.getElementById("dni").value;
-    const consulta = document.getElementById("consulta").value;
-
-    const datosEnviar = {
-      nombre: nombre,
-      apellido: apellido,
-      mail: mail,
-      dni: dni,
-      consulta: consulta
-    };
-
-    fetch('../../conexionbasededatos/update.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(datosEnviar)
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      formulario.reset(); // Restablecer el formulario después de enviar los datos
-    })
-    .catch(error => {
-      console.error(error);
-    });
-
-  });
-  formulario.addEventListener("read", (event) => {
-    event.preventDefault();
-
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const mail = document.getElementById("mail").value;
-    const dni = document.getElementById("dni").value;
-    const consulta = document.getElementById("consulta").value;
-
-    const datosEnviar = {
-      nombre: nombre,
-      apellido: apellido,
-      mail: mail,
-      dni: dni,
-      consulta: consulta
-    };
-
-    fetch('../../conexionbasededatos/read.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(datosEnviar)
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      formulario.reset(); // Restablecer el formulario después de enviar los datos
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  });
-  formulario.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const mail = document.getElementById("mail").value;
-    const dni = document.getElementById("dni").value;
-    const consulta = document.getElementById("consulta").value;
-
-    const datosEnviar = {
-      nombre: nombre,
-      apellido: apellido,
-      mail: mail,
-      dni: dni,
-      consulta: consulta
-    };
-
-    fetch('../../conexionbasededatos/insert.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(datosEnviar)
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      formulario.reset(); // Restablecer el formulario después de enviar los datos
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  });
-  
-  formulario.addEventListener("delete", (event) => {
-    event.preventDefault();
-
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const mail = document.getElementById("mail").value;
-    const dni = document.getElementById("dni").value;
-    const consulta = document.getElementById("consulta").value;
-
-    const datosEnviar = {
-      nombre: nombre,
-      apellido: apellido,
-      mail: mail,
-      dni: dni,
-      consulta: consulta
-    };
-
-    fetch('../../conexionbasededatos/delete.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(datosEnviar)
-    })
-    .then(response => response.text())
-    .then(data => {
-      console.log(data);
-      formulario.reset(); // Restablecer el formulario después de enviar los datos
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  });
-});
->>>>>>> 3c257be04c61ce71a13ff960af8cef5c7711d463:EmpresaTurismo/js/script.js
